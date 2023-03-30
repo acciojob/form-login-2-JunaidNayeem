6660e60e3ca720a729526df7442bae2f4dd96ae2
JunaidNayeem <78254107+JunaidNayeem@users.noreply.github.com>
2023-03-30 23:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function display()
+{
+var x=document.getElementById('fname').value;
+var y=document.getElementById('lname').value;
+var z=document.getElementById('phnum').value;
+var e=document.getElementById('email').value;
+alert("First Name:"+x+"\n"+"Last Name:"+y+"\n"+"Phone Number:"+z+"\n"+"Email ID:"+e);
+}
